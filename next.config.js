b47edcfa94452a88b3c90f343cf4dd3631a0d590
imjungim <jungim4201@gmail.com>
2023-07-13 14:38:50 +0900

    image파일, 이미지경로설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gogumafarm.kr",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
