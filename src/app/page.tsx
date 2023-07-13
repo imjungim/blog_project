2f115a1bac2ee4c53982ad49cc33298d67ad44ca
imjungim <jungim4201@gmail.com>
2023-07-13 14:53:34 +0900

    Markup home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import Image from 'next/image'
-
+import Image from "next/image";
+import PostCard from "./component/PostCard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>blog project</p>
-      
+    <main>
+      <section className="w-full bg-gray-300">
+        <div>
+          <div className="w-36 h-36 rounded-full bg-gray-300">
+            <Image
+              src={"/profile.webp"}
+              alt="profile-image"
+              width={200}
+              height={200}
+              className="block w-full h-auto object-cover rounded-full"
+            ></Image>
+          </div>
+          <h4>Hi😐 I'm jungim</h4>
+          <p>Frontend Developer</p>
+          <span>Contact Me📧</span>
+        </div>
+      </section>
+      <h1>Featured Post</h1>
+      <PostCard />
     </main>
-  )
+  );
 }
