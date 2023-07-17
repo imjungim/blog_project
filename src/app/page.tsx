62aca61de524f4b9f4e5452b0f22a177b4fd3dfc
imjungim <jungim4201@gmail.com>
2023-07-17 14:28:06 +0900

    markup edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,25 @@
 import Image from "next/image";
-import PostCard from "./component/PostCard";
+import PostCard from "../component/PostCard";
 
 export default function Home() {
   return (
-    <main>
-      <section className="w-full bg-gray-300">
-        <div>
-          <div className="w-36 h-36 rounded-full bg-gray-300">
-            <Image
-              src={"/profile.webp"}
-              alt="profile-image"
-              width={200}
-              height={200}
-              className="block w-full h-auto object-cover rounded-full"
-            ></Image>
-          </div>
-          <h4>Hi😐 I'm jungim</h4>
-          <p>Frontend Developer</p>
-          <span>Contact Me📧</span>
+    <section>
+      <div className="w-full bg-gray-300">
+        <div className="w-36 h-36 rounded-full bg-gray-300">
+          <Image
+            src={"/profile.webp"}
+            alt="profile-image"
+            width={200}
+            height={200}
+            className="block w-full h-auto object-cover rounded-full"
+          ></Image>
         </div>
-      </section>
+        <h4>Hi😐 I&lsquo;m jungim</h4>
+        <p>Frontend Developer</p>
+        <span>Contact Me📧</span>
+      </div>
       <h1>Featured Post</h1>
       <PostCard />
-    </main>
+    </section>
   );
 }
