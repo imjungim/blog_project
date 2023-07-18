ec1603d52a66257c468687a719ef76a8b286025e
imjungim <jungim4201@gmail.com>
2023-07-18 21:31:43 +0900

    HomePage 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
 import PostCard from "../components/PostCard";
 import Profile from "@/components/Profile";
 
-const titleList = [
-  'Featured Posts', 'Liked'
-]
 
 export default function HomePage() {
   return (
-    <section>
+    <>
       <Profile />
-      <h1>{titleList[0]}</h1>
       <PostCard />
-    </section>
+    </>
   );
 }
