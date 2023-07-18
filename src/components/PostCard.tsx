02b0a6f981a346625f933391c21d74406c99cc7d
imjungim <jungim4201@gmail.com>
2023-07-18 21:32:49 +0900

    posts.json 데이터 읽어오기 -> posts.ts , PostCard, PostsGrid.tsx 데이터 보여주기 까지

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import PostsGrid from "./PostsGrid";
+import { getAllPosts } from "@/service/posts";
 
-export default function PostCard() {
+//모든 포스트 데이터 보여주기
+export default async function PostCard() {
+  //포스트 데이터 읽어오기
+  const posts = await getAllPosts()
   return (
-    <article>
-      <div className="w-1/5 h-80 shadow-lg p-4 bg-gray-300">
+    <section>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={posts}/>
+      {/* <div className="w-1/5 h-80 shadow-lg p-4 bg-gray-300">
         <div className="">
           <Image
             src="https://gogumafarm.kr/wp-content/uploads/2023/06/%EC%B9%B4%ED%88%B0%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC-%ED%8C%8C%EC%9B%8C%ED%8D%BC%ED%94%84%EA%B1%B8-700x392.png"
@@ -19,7 +26,7 @@ export default function PostCard() {
           <p>description</p>
           <span className="p-1 bg-green-100">category</span>
         </div>
-      </div>
-    </article>
+      </div> */}
+    </section>
   );
 }
