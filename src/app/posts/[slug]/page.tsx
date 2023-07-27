89689555a0962a985da25779d8baf71897616009
imjungim <jungim4201@gmail.com>
2023-07-27 09:37:57 +0900

    posts page_ getPostData -> 해당경로에 맞는 데이터 읽어오기, 페이지에 표기

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getPostData } from "@/service/posts";
 import React from "react";
 
 type Props = {
@@ -6,6 +7,14 @@ type Props = {
   };
 };
 
-export default function Postpage({ params: { slug } }: Props) {
-  return <div>Post</div>;
+export default async function Postpage({ params: { slug } }: Props) {
+  //1. 전달된 slug에 해당하는 포스트 데이터 읽어오기
+  //2. 데이터를 마크다운뷰어에 표기
+  const post = await getPostData(slug);
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <pre>{post.content}</pre>
+    </>
+  );
 }
