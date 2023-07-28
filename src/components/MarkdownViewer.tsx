63edc292ddcecdf855e0dc261a2ec5bb441e2c2e
imjungim <jungim4201@gmail.com>
2023-07-28 17:04:19 +0900

    Add react-syntax-highlighter

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,11 +1,35 @@
+'use client'
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import ReactDom from "react-dom";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkdownRenderer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose lg:prose-xl"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              style={materialDark}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
