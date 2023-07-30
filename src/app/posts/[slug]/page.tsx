634c8735c3f9b07451c0e5954a954e4cb13d5ed9
imjungim <jungim4201@gmail.com>
2023-07-30 14:07:18 +0900

    [다음 포스트 카드]posts에서 next, prev 데이터 가져오기

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Image from "next/image";
 import PostContent from "@/components/PostContent";
 
-
 type Props = {
   params: {
     slug: string;
@@ -12,16 +11,21 @@ type Props = {
 
 export default async function Postpage({ params: { slug } }: Props) {
   const post = await getPostData(slug);
-  const { title, path } = post;
+  const { title, path, next, prev } = post;
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
-      <Image className="w-full h-1/5 max-h-[500px]"
+      <Image
+        className="w-full h-1/5 max-h-[500px]"
         src={`/images/posts/${path}.png`}
         alt={title}
         width={760}
         height={420}
       ></Image>
-      <PostContent post={post}/>
+      <PostContent post={post} />
+      <section>
+        {prev && <p>{prev.title}</p>}
+        {next && <p>{next.title}</p>}
+      </section>
     </article>
   );
 }
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -12,6 +12,8 @@ export type Post = {
 
 export type PostData = Post & {
   content: string;
+  next: Post | null;
+  prev: Post | null;
 };
 
 //featured -> true인 아이템만 filtering
@@ -33,11 +35,17 @@ export async function getAllPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), "data/posts", `${fileName}.md`);
-  const metadata = await getAllPosts().then((posts) =>
-    posts.find((post) => post.path === fileName)
-  );
-  if (!metadata)
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+
+  if (!post)
     throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
-    const content = await readFile(filePath, 'utf-8')
-    return { ...metadata, content};
+    //현재 인덱스 찾기
+    const index = posts.indexOf(post);
+    //배열에 순차적으로 0 1 2 3 4 로 쌓여질때 인덱스가 0보다 클경우 다음 포스트(최신)는 -1; 이전 포스트(오래된)는 +1
+    const next = index > 0 ? posts[index -1 ] : null;
+    const prev = index < posts.length ? posts[index + 1] : null;
+  const content = await readFile(filePath, "utf-8");
+  
+  return { ...post, content, next, prev };
 }
