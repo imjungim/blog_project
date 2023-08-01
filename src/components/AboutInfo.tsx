40dec0b8c69c7d1d5c7ca33adca361748cd42e18
imjungim <jungim4201@gmail.com>
2023-08-01 19:54:10 +0900

    ♻️ about page refactor styled

--- src/components/AboutInfo.tsx
+++ src/components/AboutInfo.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
 export default function AboutInfo() {
   return (
-    <section className="text-center bg-slate-200 shadow-md m-2 p-4">
-      <div>
-        <h2 className="font-bold py-1">Who Am I ?</h2>
-        <p>프론트엔드 개발자</p>
-      </div>
-      <div>
-        <h2 className="font-bold py-1">Career </h2>
-        <p>😂</p>
-        <p>😂</p>
-      </div>
-      <div>
-        <h2 className="font-bold py-1">Skills</h2>
-        <p>React, NextJS, NodeJS</p>
-        <p>Git, Slack</p>
-        <p>VS Code, MongoDB</p>
-      </div>
+    <section className="text-center bg-gray-100 shadow-lg m-8 p-8">
+      <h2 className={TITLE_CLASS}>Who Am I ?</h2>
+      <p>프론트엔드 개발자</p>
+
+      <h2 className={TITLE_CLASS}>Career </h2>
+      <p>😂</p>
+      <p>😂</p>
+
+      <h2 className={TITLE_CLASS}>Skills</h2>
+      <p>React, NextJS, NodeJS</p>
+      <p>Git, Slack</p>
+      <p>VS Code, MongoDB</p>
     </section>
   );
 }
