de0131818bdd6c1a8992e4361d5d8649c93f1de5
imjungim <jungim4201@gmail.com>
2023-08-01 21:48:29 +0900

    🚧 서버 라우트 작업중

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from "@/service/email";
 import * as yup from "yup";
 
 const bodySchema = yup.object().shape({
@@ -12,4 +13,11 @@ export async function POST(req: Request) {
   }
   const { from, subject, message } = req.body;
   // 노드메일러를 이용해서 메일을 전송하면 됨.
+  return sendEmail(req.body).then(
+    () => new Response(JSON.stringify({message: '메일을 성공적으로 보냄'}), status: 200)
+  ).catch(error => {
+    console.error(error)
+    return new Response
+  })
 }
+ 
