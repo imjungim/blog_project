fbfdb22453a42be74ea08e74f46bb5c4a58a4809
imjungim <jungim4201@gmail.com>
2023-09-21 21:01:15 +0900

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         <Link href="/">home🏠</Link>
         <Link href="/about">about📢</Link>
         <Link href="/posts">posts📦</Link>
-        <Link href="/contact">contact💌</Link>
+        <Link href="/contact">git test</Link>
         test
       </nav>
     </header>
